58360628289224b296bb1d8b4250760c13daf9bd
Shrejas <shrejas24@gmail.com>
2014-04-29 23:34:01 +0530

    Initial commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Shrejas on 29/04/14.
+//  Copyright (c) 2014 Shrejas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Shrejas on 29/04/14.
+//  Copyright (c) 2014 Shrejas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
